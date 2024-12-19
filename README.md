c95fd7ccc3dace7aa6f93bed7354a6da9174579f
Timothy Johnson <timothy.johnson@broadcom.com>
2024-12-19 14:52:36 -0500

    Add readme and npm bin script
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# Zowe Config Stash
+
+Wrapper for `git stash` to backup and restore Zowe config files
+
+## Installation
+
+1. Clone this repo
+2. Install globally: `npm install -g .`
+
+## Basic Usage
+
+**Warning:** This tool manages config files in `~/.zowe` and the current directory, as well as secure credentials. When applying a stash, these will be overwritten.
+
+1. (optional) Change directory to project containing `zowe.config.json`
+2. To backup, run `zc-stash push`
+3. To restore, run `zc-stash pop`
+
+## Advanced Usage
+
+**Note:** The `zc-stash` script supports the same syntax as the `git stash` command.
+
+To manage multiple stashes of Zowe config:
+
+1. Backup config with name: `zc-stash push -m "<stash name>"`
+2. List all stashes: `zc-stash list`
+3. Restore config with index: `zc-stash apply <stash index>`
--- index.js
+++ index.js
@@ -1,26 +1,27 @@
+#!/usr/bin/env node
 const childProcess = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const { ConfigUtils, CredentialManagerFactory } = require("@zowe/imperative");
 
+function copyOrRemove(srcPath, destPath) {
+    if (fs.existsSync(srcPath)) {
+        fs.copyFileSync(srcPath, destPath);
+    } else if (fs.existsSync(destPath)) {
+        fs.unlinkSync(destPath);
+    }
+}
+
 async function backupZoweConfig() {
     for (const fileName of ["zowe.config.json", "zowe.config.user.json", "zowe.schema.json"]) {
         const srcPath = path.join(ConfigUtils.getZoweDir(), fileName);
         const destPath = path.join(__dirname, "global", fileName);
-        if (fs.existsSync(srcPath)) {
-            fs.copyFileSync(srcPath, destPath);
-        } else if (fs.existsSync(destPath)) {
-            fs.unlinkSync(destPath);
-        }
+        copyOrRemove(srcPath, destPath);
     }
     for (const fileName of ["zowe.config.json", "zowe.config.user.json", "zowe.schema.json"]) {
         const srcPath = path.join(process.cwd(), fileName);
         const destPath = path.join(__dirname, "project", fileName);
-        if (fs.existsSync(srcPath)) {
-            fs.copyFileSync(srcPath, destPath);
-        } else if (fs.existsSync(destPath)) {
-            fs.unlinkSync(destPath);
-        }
+        copyOrRemove(srcPath, destPath);
     }
     const secrets = await CredentialManagerFactory.manager.load("secure_config_props");
     fs.writeFileSync(".secrets", secrets);
@@ -30,16 +31,12 @@ async function restoreZoweConfig() {
     for (const fileName of ["zowe.config.json", "zowe.config.user.json", "zowe.schema.json"]) {
         const srcPath = path.join(__dirname, "global", fileName);
         const destPath = path.join(ConfigUtils.getZoweDir(), fileName);
-        if (fs.existsSync(srcPath)) {
-            fs.copyFileSync(srcPath, destPath);
-        }
+        copyOrRemove(srcPath, destPath);
     }
     for (const fileName of ["zowe.config.json", "zowe.config.user.json", "zowe.schema.json"]) {
         const srcPath = path.join(__dirname, "project", fileName);
         const destPath = path.join(process.cwd(), fileName);
-        if (fs.existsSync(srcPath)) {
-            fs.copyFileSync(srcPath, destPath);
-        }
+        copyOrRemove(srcPath, destPath);
     }
     const secrets = fs.readFileSync(".secrets", "utf-8");
     await CredentialManagerFactory.manager.save("secure_config_props", secrets);
@@ -47,11 +44,13 @@ async function restoreZoweConfig() {
 
 (async () => {
     await CredentialManagerFactory.initialize({ service: null });
-    if (process.argv[2] === "push") {
+    const gitArgs = process.argv.slice(2);
+    if (gitArgs[0] === "push" || gitArgs[0] === "save") {
         await backupZoweConfig();
     }
-    childProcess.execSync(`git stash ${process.argv.slice(2).join(" ")}`, { stdio: "inherit" });
-    if (process.argv[2] === "apply" || process.argv[2] === "pop") {
+    childProcess.execSync(`git -C "${__dirname}" stash ${gitArgs.join(" ")}`,
+        { stdio: "inherit" });
+    if (gitArgs[0] === "apply" || gitArgs[0] === "pop") {
         await restoreZoweConfig();
     }
 })();
